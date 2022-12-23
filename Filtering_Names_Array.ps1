bae1d370446360aa3f9506333dedd63137a37e1c
Guilherme Gonçalves Pataro <88072413+PataroGuilherme@users.noreply.github.com>
2022-12-23 11:38:02 -0300

    Updated || 23-12-2022
    Updated || 23-12-2022

---
+++ Filtering_Names_Array.ps1
@@ -0,0 +1,11 @@
+﻿
+
+# 01. Filtrando nomes dentro de um array
+
+
+[String[]]$nomes = 'Ronaldy', 'Guilherme', 'Gabriel', 'Reinaldo'
+
+
+$nomes | Where-Object {$_ -like "G*"}
+
+$nomes | Where-Object {$_ -like "R*"}
