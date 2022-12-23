96156b74254137de52b2be13e449f64635b47a11
Guilherme Gonçalves Pataro <88072413+PataroGuilherme@users.noreply.github.com>
2022-12-23 11:30:53 -0300

    Updated || 23-12-2022
    Updated || 23-12-2022

---
+++ Escrevendo_Saída_Arquivo.ps1
@@ -0,0 +1,11 @@
+﻿
+Set-Location C:\Users
+
+
+$texto = Get-ChildItem | Select Name
+
+
+Write-Output $texto > C:\Users\guilherme.pataro\Downloads\lista.txt
+
+
+notepad C:\Users\guilherme.pataro\Downloads\lista.txt
